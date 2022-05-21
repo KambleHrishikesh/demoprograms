e6e80e911cf39e8b871e97ca7c139ff7683cf4b1
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-05-21 08:37:53 +0530

    LeapYear Program

---
+++ LeapYear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class LeapYear
+    {
+        public static void TheLeapYear()
+        {
+            Console.WriteLine("Enter year");
+
+            int year = int.Parse(Console.ReadLine()); 
+            //The int.Parse() method is used to convert a string to an integer value
+
+
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+                //checking for leap year or not
+            {
+                Console.WriteLine(" It is a leap year  ", year );
+            }
+            else
+            {
+                Console.WriteLine(" It is not leap year ", year);
+            }
+        }
+    }
+}
